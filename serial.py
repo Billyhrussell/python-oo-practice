26c8fc1fe97a49062f59a1734d3c881d9db0f7f2
Billyhrussell <billy.hrussell@gmail.com>
2022-07-26 16:08:35 -0700

    updating files

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Getting starting value"""
+        self.start = start #Storing starting value
+        self.new_value = start - 1 #Incrementing property
+
+    def generate(self):
+        """Adding 1 to incrementing property"""
+        self.new_value += 1
+        return self.new_value
+
+    def reset(self):
+        """Set value back to original starting value"""
+        self.new_value = self.start - 1
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,18 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, file):
+        self.file = open(file, "r")
+        self.words = self.extract_words()
+
+    def __repr__(self):
+        return f"<{len(self.words)} words read>"
+
+    def extract_words(self):
+        text = self.file.read()
+        words = text.split()
+        return words
+
+    def random(self):
+        from random import choice
+        return choice(self.words)
